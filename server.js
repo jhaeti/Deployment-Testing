5631dbd64c474e405282656bc4bd42718fcdcfa6
Ti Jhae <me@gmail.com>
2020-04-17 04:20:22 +0000

    server.js altered

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.use("/api/items", require("./routes/api/items"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 
+// Serve static assert if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // Bring mongoURI for connecting to mongodb
 const db = config.get("mongoURI");
 
@@ -28,16 +37,6 @@ mongoose
   .catch(() => console.log("Mongodb not connected"));
 
 app.get("/", (req, res) => res.json({ success: "very very true" }));
-
-// Serve static assert if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000;
 
